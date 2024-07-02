d0b0678e97883b4102ab41f05f18c72ca7ed7dc8
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-07-02 15:13:49 +0200

    added a way to filter the list by subject, need to improve it to avoid duplicates

--- src/pages/BooksList.jsx
+++ src/pages/BooksList.jsx
@@ -6,6 +6,7 @@ import "../styles/BooksList.css";
 function BooksList() {
   const [booksData, setBooksData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedSubject, setSelectedSubject] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -53,22 +54,51 @@ function BooksList() {
   if (loading) {
     return <p>Loading...</p>;
   }
+  const subjects = booksData?.map((book) =>
+    book.subjects?.map((subject) => subject.name)
+  );
+
+  console.log(subjects);
+
+  const filteredBooks = booksData.filter((book) => {
+    return selectedSubject
+      ? book.subjects.some((subject) => subject.name === selectedSubject)
+      : true;
+  });
 
   return (
-    <section className="BooksListComponent">
-      <h1> Liste des livres</h1>
-      <ul>
-        {booksData?.map((book) => (
-          <li
-            key={book.id}
-            className={book.valid ? "valid" : "invalid"}
-            onClick={() => handleBookClick(book)}
+    <>
+      <section className="BookListFilter">
+        <label>
+          Subject:
+          <select
+            value={selectedSubject}
+            onChange={(e) => setSelectedSubject(e.target.value)}
           >
-            <span>{book.displayTitle}</span>
-          </li>
-        ))}
-      </ul>
-    </section>
+            <option value="">All Subjects</option>
+            {subjects.map((subject) => (
+              <option key={subject} value={subject}>
+                {subject}
+              </option>
+            ))}
+          </select>
+        </label>
+      </section>
+      <section className="BooksListComponent">
+        <h1> Liste des livres</h1>
+        <ul>
+          {filteredBooks?.map((book) => (
+            <li
+              key={book.id}
+              className={book.valid ? "valid" : "invalid"}
+              onClick={() => handleBookClick(book)}
+            >
+              <span>{book.displayTitle}</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
 
