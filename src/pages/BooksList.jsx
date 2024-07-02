362605460e4a32ffbe1bcb156f87fb19a7f0b514
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-07-02 12:15:36 +0200

    Display books correctly with CSS applied to invalid books

--- src/pages/BooksList.jsx
+++ src/pages/BooksList.jsx
@@ -1,9 +1,73 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/BooksList.css";
+
 function BooksList() {
+  const [booksData, setBooksData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  const getListOfBooks = async () => {
+    try {
+      const res = await axios.post(
+        "https://api-preprod.lelivrescolaire.fr/graph",
+        {
+          query:
+            "query{viewer{books{hits{id displayTitle url subjects{name}levels{name}valid}}}}",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+          },
+        }
+      );
+      return res.data;
+    } catch (err) {
+      console.error("Error:", err.response ? err.response.data : err.message);
+
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const booksList = await getListOfBooks();
+      if (booksList) {
+        setBooksData(booksList.data.viewer.books.hits);
+      } else {
+        console.log("Erreur lors de la récupération des données.");
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleBookClick = (book) => {
+    if (book.valid) {
+      navigate(`/list/${book.id}`, { state: { book } });
+    }
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <section className="BooksListComponent">
-      <h1>BooksList</h1>
-
-      <p>BooksList Component</p>
+      <h1> Liste des livres</h1>
+      <ul>
+        {booksData?.map((book) => (
+          <li
+            key={book.id}
+            className={book.valid ? "valid" : "invalid"}
+            onClick={() => handleBookClick(book)}
+          >
+            <span>{book.displayTitle}</span>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
