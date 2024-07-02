687d53593f33b9cbdbaef68245ed797199db097b
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-07-02 12:18:25 +0200

    Redirect to book details page with correct informations on book click.

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,9 +1,15 @@
+import { Link, useLocation } from "react-router-dom";
+import "../styles/Book.css";
+
 function Book() {
+  const location = useLocation();
+  const { book } = location.state;
+
   return (
     <section className="BookComponent">
-      <h1>Book</h1>
-
-      <p>Book Component</p>
+      <h1>{book.displayTitle}</h1>
+      <img src={book.url} alt={book.displayTitle} />
+      <Link to="/list">Retourner à la liste</Link>
     </section>
   );
 }
