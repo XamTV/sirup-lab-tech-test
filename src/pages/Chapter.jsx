1905703568374d553d4d679dfb2030867f2e4057
Maxime MAUSSION <maximemaussion30@gmail.com>
2024-07-02 13:50:25 +0200

    Selected chapter now redirect to his page

---
+++ src/pages/Chapter.jsx
@@ -0,0 +1,11 @@
+function Chapter() {
+  return (
+    <section className="ChapterComponent">
+      <h1>Chapter</h1>
+
+      <p>Chapter Component</p>
+    </section>
+  );
+}
+
+export default Chapter;
